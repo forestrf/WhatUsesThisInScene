0d3648dba1d8b48afa60878d43b9e78560e16e64
Andres <forestrf@gmail.com>
2020-06-14 22:23:55 +0200

    First commit

---
+++ Editor/README.md
@@ -0,0 +1,9 @@
+# WhatUsesThisInScene
+
+Some UnityEvent or script, or anything, is pointing to some other thing in the scene and applying changes to it, but you can't find who? Well, this won't tell you who is doing it, but will list all the fields that point to that object in the scene, making it easier to find the culprit.
+
+# How to use it
+- Right click on a GameObject in the Hierarchy Window, or click on the cog (or three dots) on a component in the Inspector Window, and click "What Uses This In The Scene".
+- Check the results on the Console Window.
+
+Inspired by https://github.com/Facepunch/WhatUsesThis
---
+++ Editor/WhatUsesThisInScene.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class WhatUsesThisInScene {
+	[MenuItem("GameObject/What Uses This In The Scene", false, 10)]
+	[MenuItem("CONTEXT/Component/What Uses This In The Scene", false, 10)]
+	static void Execute() {
+		var allComponents = GameObject.FindObjectsOfType<Component>();
+
+		List<Object> thisObj = new List<Object>();
+
+		int iCount = 0;
+
+		var selectedObj = Selection.activeObject;
+		string selected = selectedObj.name;
+
+		Debug.Log($"<color=#5C93B9>What uses <b>{selected}</b>?</color>", selectedObj);
+
+		if (selectedObj is GameObject go) {
+			Debug.Log($"<color=#5C93B9>The object is a GameObject. Will search for the components it contains too.</color>");
+			thisObj.AddRange(go.GetComponents<Component>());
+		}
+
+		bool cancelled = false;
+
+		try {
+			for (int i = 0; i < allComponents.Length; i++) {
+				if (Progress(i, allComponents.Length, 0, iCount)) {
+					cancelled = true;
+					goto End;
+				}
+
+				SerializedObject obj = new SerializedObject(allComponents[i]);
+
+				SerializedProperty props = obj.GetIterator();
+				int j = 0;
+				while (props.Next(true)) {
+					Object objRef = null;
+					switch (props.propertyType) {
+						case SerializedPropertyType.ExposedReference:
+							objRef = props.exposedReferenceValue;
+							break;
+						case SerializedPropertyType.ObjectReference:
+							objRef = props.objectReferenceValue;
+							break;
+					}
+					if (objRef == null) continue;
+
+					if (Progress(i, allComponents.Length, j, iCount)) {
+						cancelled = true;
+						goto End;
+					}
+
+					if (thisObj.Contains(objRef)) {
+						string d = allComponents[i].gameObject.name + "/" + allComponents[i].GetType();
+						Debug.Log($"<color=#8CA166>  {d}</color>", allComponents[i]);
+						iCount++;
+					}
+					j++;
+				}
+			}
+		}
+		finally { }
+
+		End:
+		EditorUtility.ClearProgressBar();
+
+		string status = cancelled ? "cancalled" : "complete";
+		Debug.Log($"<color=#5C93B9>Search {status}, found <b>{iCount}</b> result{(iCount == 1 ? "" : "s")}</color>");
+	}
+
+	static bool Progress(int i, int length, int j, int f) {
+		return EditorUtility.DisplayCancelableProgressBar("Searching...", "Found in " + f + " fields. Components inspected: " + i + " / " + length + " (Fields inspected " + j + ")", i / (length - 1f));
+	}
+}
